159ddfebbc5fcaba429a47be80e5dec4c8982323
FlutLab.io <>
2022-05-31 04:41:41 +0000

    Autosave

--- lib/calls.dart
+++ lib/calls.dart
@@ -20,11 +20,6 @@ class Calls extends StatelessWidget {
             ),
 
             // Masukkan Coding disini jangan rubah lainnya
-                    child: ListView(
-          children: [
-            SizedBox(
-              height: 10,
-            ),
             ListTile(
                 leading: Stack(
                   alignment: AlignmentDirectional.bottomEnd,
@@ -63,47 +58,6 @@ class Calls extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            getContainer("Recent updates"),
-            SizedBox(
-              height: 10,
-            ),
-            getStory("Sasuke", "Just now"),
-            getStory("Ronaldo", "Today, 2:8 pm"),
-            getStory("Moh salah", "6 minutes ago"),
-            getStory("Rossi", "25 minutes ago"),
-            getStory("Torres", "Yestrday, 8:45 pm"),
-            getStory("Omar Hassan", "Yestrday, 5:5 Am"),
-            SizedBox(
-              height: 10,
-            ),
-            getContainer("Viewed updates"),
-            SizedBox(
-              height: 10,
-            ),
-            getStory("Hassan fares", "25 minutes ago"),
-            ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage("https://asset.kompas.com/crops/-EE26_TNXjiiiz5NP-cvLblAJX4=/0x0:946x631/750x500/data/photo/2020/09/23/5f6ab981c74e4.jpg"),
-                radius: 30,
-              ),
-              title: Text(
-                "Carlos",
-                style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
-              ),
-              subtitle: Text("Today, 2:54 pm"),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            getContainer("Muted updets"),
-            SizedBox(
-              height: 10,
-            ),
-            getStory("Karim Ayman", "54 minutes ago"),
-            getStory("Hady Nasr", "15 minutes ago"),
-            getStory("Hager Ahmed", "Yestrday, 5:5 Am"),
-          ],
-        ),
           ],
         ),
       ),
